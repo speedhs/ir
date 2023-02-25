d54ab8682c2eb130a0eecddb19e34a6c84709deb
speedhs <64911433+speedhs@users.noreply.github.com>
2023-02-25 22:30:04 +0000

    i am tired

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import math
+def compute_cosine_similarity(query_weights, doc_weights):
+    dot_product = 0
+    for term in query_weights:
+        if term in doc_weights:
+            dot_product += query_weights[term] * doc_weights[term]
+    query_norm = math.sqrt(sum([weight ** 2 for weight in query_weights.values()]))
+    doc_norm = math.sqrt(sum([weight ** 2 for weight in doc_weights.values()]))
+    if query_norm == 0 or doc_norm == 0:
+        return 0
+    return dot_product / (query_norm * doc_norm)
+
 def rank_documents(query_weights, index, doc_lengths):
     doc_scores = {}
     for term in query_weights:
